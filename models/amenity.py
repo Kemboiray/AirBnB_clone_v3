649f750bd9a8782162481bd31166a365727dc021
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-07-31 00:42:34 +0300

    Update remote

--- models/amenity.py
+++ models/amenity.py
@@ -9,10 +9,9 @@ from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-    """Representation of Amenity"""
-
-    if models.storage_t == "db":
-        __tablename__ = "amenities"
+    """Representation of Amenity """
+    if models.storage_t == 'db':
+        __tablename__ = 'amenities'
         name = Column(String(128), nullable=False)
     else:
         name = ""
