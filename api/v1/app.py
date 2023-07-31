f27b5cbc7c32342ea45f4915d38fd76930ab1819
Patrick-052 <muiruripatrick052@gmail.com>
2023-07-31 13:40:24 +0300

    api debug

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,20 +2,23 @@
 """Creating a web application and
    impressing blueprints on it"""
 
+import os
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
-app.register_blueprint(app_views, url_prefix='/api/v1')
+app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def session_teardown():
+def session_teardown(exc):
     """Closing the current session"""
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host='HBNB_API_HOST', port='HBNB_API_PORT',
+    host = os.getenv('HBNB_API_HOST')
+    port = os.getenv('HBNB_API_PORT')
+    app.run(host=host, port=port,
             debug=False, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """Checking the response status code"""
 
-from app_views import api.v1.views
+from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route('/status')
+@app_views.route('/status', strict_slashes=False, methods=['GET'])
 def response_status():
     """Returning the status of the response"""
-    return jsonify(({"status": "OK"}), 200)
+    return jsonify({"status": "OK"})
