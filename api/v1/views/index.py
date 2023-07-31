8b66dc18004a6799817947d11313867465b2d057
Patrick-052 <muiruripatrick052@gmail.com>
2023-07-31 14:33:47 +0300

    count retrieval in json

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,10 +2,30 @@
 """Checking the response status code"""
 
 from api.v1.views import app_views
+from models import storage
 from flask import jsonify
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', strict_slashes=False, methods=['GET'])
 def response_status():
     """Returning the status of the response"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False, methods=['GET'])
+def object_count():
+    """Retrieve object count"""
+    classes = {"amenities": Amenity, "cities": City, "places": Place,
+               "reviews": Review, "states": State, "users": User}
+
+    obj_count = {}
+    for k, v in classes.items():
+        count = storage.count(v)
+        obj_count[k] = count
+    return jsonify(obj_count)
